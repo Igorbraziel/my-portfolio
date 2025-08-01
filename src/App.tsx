ebaf8a34389aa176667e06f4ee02c72469028548
braziel <braziel@discente.ufg.br>
2025-08-01 12:40:49 -0300

    Implementing the App Routes with React Routes and creating the Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import AppLayout from "./components/layout/AppLayout";
+
 import { DarkModeProvider } from "./context/DarkModeContext";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectPage from "./pages/ProjectPage";
+import PageNotFound from "./pages/PageNotFound";
+import SkillsPage from "./pages/SkillsPage";
 
 function App() {
   return (
     <DarkModeProvider>
-      <AppLayout>
-        <div className="font-extrabold text-xl underline">Hello Tailwind</div>
-      </AppLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="/homepage" />} />
+            <Route path="homepage" element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="contact" element={<ContactPage />} />
+            <Route path="projects">
+              <Route index element={<ProjectsPage />} />
+              <Route path=":pid" element={<ProjectPage />} />
+            </Route>
+            <Route path="skills" element={<SkillsPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </DarkModeProvider>
   );
 }
--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,11 +1,12 @@
 import type { PropsWithChildren } from "react";
 import NavigationBar from "./NavigationBar";
+import { Outlet } from "react-router";
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
     <div className="dark:bg-black text-gray-400">
       <NavigationBar />
-      <span>{children} App Layout</span>
+      <Outlet />
     </div>
   );
 }
