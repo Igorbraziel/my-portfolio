95c1ee0418f393bd7544c469251f475af063f968
braziel <braziel@discente.ufg.br>
2025-08-07 15:01:38 -0300

    Implementing a very useful useMediaQuery custom Hook

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,13 +1,17 @@
+import useMediaQuery from "../../hooks/useMediaQuery";
 import DarkModeButton from "../ui/DarkModeButton";
+import Logo from "../ui/Logo";
 import Footer from "./Footer";
 import NavigationBar from "./NavigationBar";
 import { Outlet } from "react-router";
 
 export default function AppLayout() {
+  const smallBreakpoint = useMediaQuery("(min-width: 640px)");
+
   return (
     <div className="min-h-screen bg-slate-100 text-neutral-900 sm:pt-4 lg:pt-6 xl:pt-7 dark:bg-slate-900 dark:text-neutral-100">
       <header className="flex w-full items-center justify-between bg-slate-200 px-4">
-        <NavigationBar />
+        {smallBreakpoint ? <NavigationBar /> : <Logo />}
         <DarkModeButton />
       </header>
       <Outlet />
